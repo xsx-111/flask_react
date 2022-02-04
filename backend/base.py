d824afac56b6f4f4abe70cec5f2e5b4bd627cfe5
xsx <xiang15987jiang@gmail.com>
2022-02-04 01:40:36 +0000

    send data to backend

--- backend/base.py
+++ backend/base.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 api = Flask(__name__)
 
-@api.route('/profile')
-def my_profile():
+@api.route('/profile/<searchResult>')
+def my_profile(searchResult):
     response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+        "name": searchResult,
+        "about": "Hello! I'm a full stack developer that loves python and javascript"
     }
 
     return response_body
