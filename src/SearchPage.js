4e765c3b0bbaf52ef5c5ff56287aa7a3daf7a535
xsx <xiang15987jiang@gmail.com>
2022-02-28 15:52:19 +0000

    pagination

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -11,24 +11,21 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
+import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
-import { styled } from '@mui/material/styles';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Pagination from '@mui/material/Pagination';
 import { useEffect, useState } from 'react';
 import axios from "axios";
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 function Copyright() {
     return (
@@ -43,76 +40,63 @@ function Copyright() {
     );
 }
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const Theme = { palette: { primary: { main: '#e0b159' }, secondary: { main: '#FFFFFF' } } };
 const theme = createTheme(Theme);
 
-
-//constant for the self-defined selector
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-    'label + &': {
-        marginTop: theme.spacing(3),
-    },
-    '& .MuiInputBase-input': {
-        //圆角的半径
-        borderRadius: 4,
-        position: 'relative',
-        backgroundColor: '#f3dda6',
-        border: '0px solid #efd697',
-        fontSize: 10,
-        padding: '8px 26px 10px 5px',
-        transition: theme.transitions.create(['border-color', 'box-shadow']),
-        // Use the system font instead of the default Roboto font.
-        fontFamily: [
-            '-apple-system',
-            'BlinkMacSystemFont',
-            '"Segoe UI"',
-            'Roboto',
-            '"Helvetica Neue"',
-            'Arial',
-            'sans-serif',
-            '"Apple Color Emoji"',
-            '"Segoe UI Emoji"',
-            '"Segoe UI Symbol"',
-        ].join(','),
-        '&:focus': {
-            borderRadius: 4,
-            borderColor: '#efd697',
-            boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-        },
-    },
-}));
-
 export default function SearchPage() {
 
-    const [displayNum, setDisplayNum] = React.useState('');
-    const handleChange = (event) => {
-        setDisplayNum(event.target.value);
+    const [offset, setOffset] = useState(0);
+    const [perpage, setPerpage] = useState(10);
+    const [searchContent, setSearchContent] = useState('');
+
+    const handleChange = (event, value) => {
+        setOffset(perpage*(value-1));
+    };
+
+    const handleNumChange = (event) => {
+        setPerpage(event.target.value);
     };
 
     const [profileData, setProfileData] = useState([{}]);
     const location = useLocation();
 
     useEffect(() => {
-        async function getSearchResult() {
+        async function getSearchResult(state) {
         axios({
             method: "GET",
-            url: `/profile/${location.state}`,
+            url: `/profile/${state}`,
         })
         .then((response) => {
             setProfileData(response.data)
             console.log(response.data)
         }).catch((error) => {
             if (error.response) {
-            console.log(error.response)
-            console.log(error.response.status)
-            console.log(error.response.headers)
+                console.log(error.response)
+                console.log(error.response.status)
+                console.log(error.response.headers)
             }
         })
         }
-        getSearchResult();
+        getSearchResult(location.state);
         console.log(profileData);
     }, [])
+
+    function getSearchResultData() {
+        axios({
+            method: "GET",
+            url: `/profile/${searchContent}`,
+        })
+        .then((response) => {
+            setProfileData(response.data)
+            console.log(response.data)
+        }).catch((error) => {
+            if (error.response) {
+                console.log(error.response)
+                console.log(error.response.status)
+                console.log(error.response.headers)
+            }
+        })
+    }
 
     return (
         <ThemeProvider theme={theme}>
@@ -127,38 +111,15 @@ export default function SearchPage() {
                             sx={{ ml: 1, flex: 1 }}
                             placeholder="input lyrics you want to search"
                             defaultValue={location.state}
-                            inputProps={{ 'aria-label': 'search google maps' }}
+                            onChange={e => setSearchContent(e.target.value)}
                         />
-                        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                        <IconButton sx={{ p: '10px' }} aria-label="search" onClick={e => getSearchResultData()}>
                             <SearchIcon />
                         </IconButton>
-                    </Paper><div>
-                        <FormControl sx={{ m: 1 }} variant="filled" size={'small'}>
-                            <InputLabel htmlFor="demo-customized-textbox">diaplayNum</InputLabel>
-                            <BootstrapInput id="demo-customized-textbox" />
-                        </FormControl>
-                        <FormControl sx={{ m: 1 }} variant="standard">
-                            <InputLabel id="demo-customized-select-label">displayNum</InputLabel>
-                            <Select
-                                labelId="demo-customized-select-label"
-                                id="demo-customized-select"
-                                value={displayNum}
-                                onChange={handleChange}
-                                input={<BootstrapInput />}
-                            >
-                               <MenuItem value="">
-                                    <em>None</em>
-                                </MenuItem>
-                                <MenuItem value={5}>5</MenuItem>
-                                <MenuItem value={10}>10</MenuItem>
-                                <MenuItem value={15}>15</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </div>
+                    </Paper>
                 </Toolbar>
             </AppBar>
             <main>
-                {/* Hero unit */}
                 <Box
                     sx={{
                         bgcolor: '#efd697',
@@ -172,28 +133,16 @@ export default function SearchPage() {
                             variant="h3"
                             align="center"
                             color="text.primary"
-                            gutterBottom
+                            sx={{mt: 5}}
                         >
                             Song Results
                         </Typography>
-                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
-
-                        </Typography>
-                        <Stack
-                            sx={{ pt: 4 }}
-                            direction="row"
-                            spacing={2}
-                            justifyContent="center"
-                        >
-                            <Button variant="contained">Main call to action</Button>
-                            <Button variant="outlined">Secondary action</Button>
-                        </Stack>
                     </Container>
                 </Box>
                 <Container sx={{ py: 8 }}>
                     {/* End hero unit */}
                     <Grid container spacing={2}>
-                        {profileData.map((result) => (
+                        {profileData.slice(offset, offset + perpage).map((result) => (
                             <Grid item key={result.id} xs={12} sm={6} md={4}>
                                 <Card
                                     sx={{ height: 400 }}
@@ -204,13 +153,6 @@ export default function SearchPage() {
                                         height="220"
                                         image={result.image}
                                         alt="album image"
-                                        // component="img"
-                                        // sx={{
-                                        //     // 16:9
-                                        //     pt: '56.25%',
-                                        // }}
-                                        // image={result.image}
-                                        // alt="random"
                                     />
                                     <CardContent sx={{ flexGrow: 1 }}>
                                         <Typography gutterBottom variant="h5" component="h2">
@@ -230,6 +172,33 @@ export default function SearchPage() {
                             </Grid>
                         ))}
                     </Grid>
+                    <Stack direction="row" spacing={2} sx={{mt: 10}} justifyContent="center">
+                        <Pagination
+                            limit={perpage}
+                            offset={offset}
+                            count={Math.ceil(profileData.length/perpage)}
+                            onChange={handleChange}
+                            showFirstButton showLastButton
+                            sx={{ height: 20, mt: 1 }}
+                        />
+                        <Box sx={{ width: 70, height: 20 }}>
+                            <FormControl>
+                                <InputLabel id="demo-simple-select-label">number</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={perpage}
+                                        label="num"
+                                        onChange={handleNumChange}
+                                    >
+                                        <MenuItem value={10}>10</MenuItem>
+                                        <MenuItem value={30}>30</MenuItem>
+                                        <MenuItem value={50}>50</MenuItem>
+                                        <MenuItem value={100}>100</MenuItem>
+                                    </Select>
+                            </FormControl>
+                        </Box>
+                    </Stack>
                 </Container>
             </main>
             <Box sx={{ bgcolor: '#efd697', p: 6 }} component="footer">
