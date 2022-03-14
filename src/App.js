cfe613496171b16a7fc3925a3162c07f4129f582
xsx <xiang15987jiang@gmail.com>
2022-03-14 14:30:07 +0000

    add advance search

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import {createTheme, ThemeProvider} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
 import * as React from "react";
 import Link from "@mui/material/Link";
 
@@ -24,14 +30,22 @@ function Copyright() {
 export default function App() {
 
   const [fName, setfName] = useState('');
+  const [searchType, setSearchType] = useState('Lyrics');
   const navigate = useNavigate();
 
   const Theme = { palette: { primary: { main: '#f1d692' }, secondary: { main: '#FFFFFF' } } };
   const theme = createTheme(Theme);
 
   function getData() {
-    navigate("/SearchPage", {state: fName});
-  }
+    navigate("/SearchPage", {state: {
+        query: fName,
+        type: searchType
+    }});
+    }
+
+  const handleTypeChange = (event) => {
+    setSearchType(event.target.value);
+    };
   
   return (
     <ThemeProvider theme={theme}>
@@ -41,18 +55,40 @@ export default function App() {
                     <div className={"App-title"}>
                         Lyrics Search
                     </div>
-                    <div>
-                        <input 
-                            onKeyPress={(event) => {
-                                if (event.key === 'Enter') {
-                                    getData()
-                                }
-                            }}
-                            required type="text" placeholder="Type a query" className={"App-input"} onChange={e => setfName(e.target.value)}/>
-                        <button onClick={e => getData()} className={"App-submit"}> 
-                            Search 
-                        </button>
-                    </div>
+                    <Stack direction="row" spacing={2} sx={{mt: 10}} justifyContent="center">
+                        <Box sx={{ width: 70, height: 20, mr: 8 }}>
+                            <FormControl>
+                                <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={searchType}
+                                        label="num"
+                                        onChange={handleTypeChange}
+                                    >
+                                        <MenuItem value={"Lyrics"}>Lyrics</MenuItem>
+                                        <MenuItem value={"song_name_preprocess"}>Song Name</MenuItem>
+                                        <MenuItem value={"album_name_preprocess"}>Album Name</MenuItem>
+                                        <MenuItem value={"artist_name_preprocess"}>Singer Name</MenuItem>
+                                        <MenuItem value={"genres_preprocess"}>Genres</MenuItem>
+                                    </Select>
+                            </FormControl>
+                        </Box>
+                        <Grid>
+                            <div>
+                                <input 
+                                    onKeyPress={(event) => {
+                                        if (event.key === 'Enter') {
+                                            getData()
+                                        }
+                                    }}
+                                    required type="text" placeholder="Type a query" className={"App-input"} onChange={e => setfName(e.target.value)}/>
+                                <button onClick={e => getData()} className={"App-submit"}> 
+                                    Search 
+                                </button>
+                            </div>
+                        </Grid>
+                    </Stack>
                 </header>
                 <Box sx={{ bgcolor: '#efd697', p: 6 }} component="footer">
                     <Typography variant="h6" align="center" gutterBottom>
